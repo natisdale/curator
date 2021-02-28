edaf48c715a7e1536a6dad2e6a182b2f87fc5f39
Nathan Tisdale <natisdale@mail.csuchico.edu>
2021-02-27 23:16:14 -0800

    Adding initial unit tests

--- curator_test.py
+++ curator_test.py
@@ -1,22 +1,52 @@
+import pytest
 import curator
 
-def test():
-    user = curator.User("flast")
-    assert "flast" == user.getName()
+# Using xunit style setup and tear down
+class TestClass:
+    @classmethod
+    def setup_class(cls):
+       pass
 
-    museum = curator.Museum(
-        name = "Metropolitan Museum", 
-        searchUrlBase = "https://collectionapi.metmuseum.org/public/collection/v1/search?",
-        objectUrlBase = "https://collectionapi.metmuseum.org/public/collection/v1/objects/"
-    )
-    assert museum.getSearchUrlBase() == "https://collectionapi.metmuseum.org/public/collection/v1/search?"
-    assert museum.getObjectUrlBase() == "https://collectionapi.metmuseum.org/public/collection/v1/objects/"
+    @classmethod
+    def teardown_class(cls):
+        pass
+    
+    def setup_method(self, method):
+        if method == self.testNewUser:
+            self.username = "Test User"
+        else:
+            pass
 
-    query = curator.Query(museum)
-    query.setParameter("isOnView", "true")
-    query.setParameter("title", "The Laundress")
-    query.setParameter("q", "Daumier")
-    response = query.runQuery()
-    assert len(response) > 0
-    user.favorites.append(response[0])
-    assert user.favorites[0].getTitle() == "The Laundress"
+    def teardown_method(self, method):
+        if method == self.testNewUser:
+            del self.username
+        else:
+            pass
+        
+    def testNewUser(self):
+        user = curator.User(self.username)
+        assert user.getName() == self.username
+        assert len(user.favorites) == 0
+        del user
+
+ 
+    def testHappyPath(self):
+        user = curator.User("flast")
+        assert "flast" == user.getName()
+        museum = curator.Museum(
+            name = "Metropolitan Museum", 
+            searchUrlBase = "https://collectionapi.metmuseum.org/public/collection/v1/search?",
+            objectUrlBase = "https://collectionapi.metmuseum.org/public/collection/v1/objects/"
+        )
+        assert museum.getSearchUrlBase() == "https://collectionapi.metmuseum.org/public/collection/v1/search?"
+        assert museum.getObjectUrlBase() == "https://collectionapi.metmuseum.org/public/collection/v1/objects/"
+
+        query = curator.Query(museum)
+        query.setParameter("isOnView", "true")
+        query.setParameter("title", "true")
+        query.setParameter("q", "The Laundress")
+        response = query.runQuery()
+        assert len(response) > 0
+        user.favorites.append(response[0])
+        assert user.favorites[0].getTitle() == "The Laundress"
+ 
