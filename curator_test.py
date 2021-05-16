c3657d7d35a8ba5322a16c14f6115c5c039b3d1e
Nathan Tisdale <natisdale@mail.csuchico.edu>
2021-05-16 13:11:20 -0700

    Adding back curator_test.py

---
+++ curator_test.py
@@ -0,0 +1,51 @@
+import pytest
+import curator
+
+# Using xunit style setup and tear down
+class TestClass:
+    @classmethod
+    def setup_class(cls):
+       pass
+
+    @classmethod
+    def teardown_class(cls):
+        pass
+    
+    def setup_method(self, method):
+        if method == self.testNewUser:
+            self.username = "Test User"
+        else:
+            pass
+
+    def teardown_method(self, method):
+        if method == self.testNewUser:
+            del self.username
+        else:
+            pass
+        
+    def testNewUser(self):
+        user = curator.User(self.username)
+        assert user.getName() == self.username
+        assert len(user.getFavorites()) == 0
+        del user
+
+ 
+    def testHappyPath(self):
+        user = curator.User("flast")
+        assert "flast" == user.getName()
+        museum = curator.Museum(
+            name = "Metropolitan Museum", 
+            searchUrlBase = "https://collectionapi.metmuseum.org/public/collection/v1/search?",
+            objectUrlBase = "https://collectionapi.metmuseum.org/public/collection/v1/objects/"
+        )
+        assert museum.getSearchUrlBase() == "https://collectionapi.metmuseum.org/public/collection/v1/search?"
+        assert museum.getObjectUrlBase() == "https://collectionapi.metmuseum.org/public/collection/v1/objects/"
+
+        query = curator.Query(museum)
+        query.setParameter("isOnView", "true")
+        query.setParameter("title", "true")
+        query.setParameter("q", "The Laundress")
+        response = query.runQuery()
+        assert len(response) > 0
+        user.addFavorite(response[0])
+        assert user.getFavorites().pop().getTitle() == "The Laundress"
